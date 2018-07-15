303f51cef0e39e1f14972ab5c8ba30fb9c02188b
efraim krug <efraimmkrug@gmail.com>
2018-07-15 14:28:14 -0400

    First BeisChayim

---
+++ db01.js
@@ -0,0 +1,6 @@
+var YahrzeitList = '{ "Yahrzeits": [' +
+'{"Name":"Bill Schmaltz" , "HName": "Baruch ben Moishe", "EDate" : "December 3, 2017", "HDate" : "15 Kislev, 5778", ' +
+'"Pic01" : "soldiers001.jpeg", "Pic02" : "elderly001.jpeg", "Comments01": "Grandpa was an amazing man who did amazing things, including the time when he..."},' +
+'{"Name":"Fred Haddock" ,  "HName": "Fishel ben Zevulun", "EDate" : "July 18, 2016", "HDate" : "12 Tammuz, 5776", ' + 
+'"Pic01" : "soldiers002.jpeg", "Pic02" : "elderly002.jpeg", "Comments01": "I miss my father. Dad was... But I am really happy he married my mom."}]}';
+		    
