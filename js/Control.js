8d30b0cb74ab9b3272a9ad11807ae6d2655099f2
Efraim Krug <efraimmkrug@gmail.com>
2018-07-31 23:48:40 -0400

    adding a few small things

--- js/Control.js
+++ js/Control.js
@@ -1,19 +1,27 @@
 // Control.js
 var currentMonth;
 var backgroundTrack = 0;
+var showButton = false;
 
 // ========== selecting the month ======================
 function showIt(){
 	var month = document.getElementById("monthSelect");
-	month.style.display = 'inline';
 	var action = document.getElementById("actionSelect");
-	action.style.display = 'inline';
+	if(showButton){
+		action.style.display = 'inline';
+		month.style.display = 'inline';
+		showButton = false;
+	} else {
+		action.style.display = 'none';
+		month.style.display = 'none';
+		showButton = true;
+	}
 	//alert(month.value);
 }
 
 function doit(){
 	var month = document.getElementById("monthSelect");
-	month.style.display = 'none';
+	//month.style.display = 'none';
 	currentMonth = month.value;
 	//alert(month.value);
 }
