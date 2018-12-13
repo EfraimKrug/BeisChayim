9444e5f6a7bb686785b09f3345f93a342165cf99
efraim krug <efraimmkrug@gmail.com>
2018-12-13 18:51:54 -0500

    fixing filenames

--- ShellScripts/BChStep01.sh
+++ ShellScripts/BChStep01.sh
@@ -4,31 +4,31 @@
 # remember - this is running without getting new data
 # from shulcloud...
 ###############################################################################
-echo "BChCycle01: "  >> $HOME/code/BeisChayim/log/CycleLog
-date >> $HOME/code/BeisChayim/log/CycleLog
-[ -n "$(find $HOME/Downloads -name '\$\$BC\$\$*' | head -1)" ] && mv $HOME/Downloads/\$\$BC\$\$* $HOME/code/BeisChayim/data
+echo "BChCycle01: "  >> $HOME/bcCode/BeisChayim/log/CycleLog
+date >> $HOME/bcCode/BeisChayim/log/CycleLog
+[ -n "$(find $HOME/Downloads -name '\$\$BC\$\$*' | head -1)" ] && mv $HOME/Downloads/\$\$BC\$\$* $HOME/bcCode/BeisChayim/data
 
 # move files 5 cycles back...
 
-mv $HOME/code/BeisChayim/data/work/db01-4.js $HOME/code/BeisChayim/data/work/db01-5.js
-mv $HOME/code/BeisChayim/data/work/db01-3.js $HOME/code/BeisChayim/data/work/db01-4.js
-mv $HOME/code/BeisChayim/data/work/db01-2.js $HOME/code/BeisChayim/data/work/db01-3.js
-mv $HOME/code/BeisChayim/data/work/db01-1.js $HOME/code/BeisChayim/data/work/db01-2.js
-mv $HOME/code/BeisChayim/data/work/db01-0.js $HOME/code/BeisChayim/data/work/db01-1.js
+mv $HOME/bcCode/BeisChayim/data/work/db01-4.js $HOME/bcCode/BeisChayim/data/work/db01-5.js
+mv $HOME/bcCode/BeisChayim/data/work/db01-3.js $HOME/bcCode/BeisChayim/data/work/db01-4.js
+mv $HOME/bcCode/BeisChayim/data/work/db01-2.js $HOME/bcCode/BeisChayim/data/work/db01-3.js
+mv $HOME/bcCode/BeisChayim/data/work/db01-1.js $HOME/bcCode/BeisChayim/data/work/db01-2.js
+mv $HOME/bcCode/BeisChayim/data/work/db01-0.js $HOME/bcCode/BeisChayim/data/work/db01-1.js
 
-cp $HOME/code/BeisChayim/js/db01.js $HOME/code/BeisChayim/data/work/db01-0.js
+cp $HOME/bcCode/BeisChayim/js/db01.js $HOME/bcCode/BeisChayim/data/work/db01-0.js
 
 # create the new db01.js
-#cp $HOME/code/BeisChayim/data/out02 $HOME/code/BeisChayim/js/db01.js
-cd $HOME/code/BeisChayim
-python $HOME/code/BeisChayim/python/collect.py > $HOME/code/BeisChayim/data/out03
+#cp $HOME/bcCode/BeisChayim/data/out02 $HOME/bcCode/BeisChayim/js/db01.js
+cd $HOME/bcCode/BeisChayim
+python $HOME/bcCode/BeisChayim/python/collect.py > $HOME/bcCode/BeisChayim/data/out03
 
 # store all the update files
-[ -n "$(find $HOME/code/BeisChayim/data -name '\$\$BC\$\$*' | head -1)" ] && mv $HOME/code/BeisChayim/data/\$\$BC\$\$* $HOME/code/BeisChayim/data/used
-cp $HOME/code/BeisChayim/data/out03 $HOME/code/BeisChayim/js/db01.js
-cp $HOME/code/BeisChayim/data/out03 $HOME/code/BeisChayim/data/out02
+[ -n "$(find $HOME/bcCode/BeisChayim/data -name '\$\$BC\$\$*' | head -1)" ] && mv $HOME/bcCode/BeisChayim/data/\$\$BC\$\$* $HOME/bcCode/BeisChayim/data/used
+cp $HOME/bcCode/BeisChayim/data/out03 $HOME/bcCode/BeisChayim/js/db01.js
+cp $HOME/bcCode/BeisChayim/data/out03 $HOME/bcCode/BeisChayim/data/out02
 
 # restart browswer
 pkill -f firefox
-firefox $HOME/code/BeisChayim/beisChayim.html &
+firefox $HOME/bcCode/BeisChayim/beisChayim.html &
 ###############################################################################
