1d7a55c92aeb0352c744777ecf830a6a6fa1b75a
efraim krug <efraimmkrug@gmail.com>
2018-12-16 15:57:05 -0500

    fixing shellscripts

---
+++ ShellScripts/ShellInstall.sh
@@ -0,0 +1,10 @@
+# reinstall shell scripts
+cp $HOME/bcCode/beisChayim/python/ShellScripts/*.sh $HOME/bin
+cd $HOME/bin
+mv $HOME/bin/appInstall.sh $HOME/bin/appInstall
+mv $HOME/bin/BChCycle01.sh $HOME/bin/_BChCycle01
+#mv $HOME/BChCycle02.sh $HOME/BChCycle02
+#mv $HOME/BChStep01.sh $HOME/BChStep01
+##### mv $HOME/bin/ShellInstall.sh $HOME/bin/ShellInstall
+mv $HOME/bin/ConfigInstall.sh $HOME/bin/_ConfigInstall
+chmod 555 $HOME/bin/*.sh
