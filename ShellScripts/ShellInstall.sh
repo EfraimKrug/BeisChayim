15e02efb12d45fe2e43eee820e2cd41ef38af98f
efraim krug <efraimmkrug@gmail.com>
2018-12-16 18:57:26 -0500

    last changes

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -1,8 +1,10 @@
 # reinstall shell scripts
+cd $HOME/bin
 cp $HOME/bcCode/BeisChayim/ShellScripts/*.sh $HOME/bin
 cp $HOME/bcCode/BeisChayim/ShellScripts/startUp $HOME/bin
-cd $HOME/bin
-chmod 555 $HOME/bin/*.sh
-chmod 555 $HOME/bin/startUp
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
 mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
+
+chmod 555 $HOME/bin/BChCycle01
+chmod 555 $HOME/bin/ConfigInstall
+chmod 555 $HOME/bin/startUp
