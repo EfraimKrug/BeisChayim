a9ac0c150456bec50d73a0c84e13533d8d09606a
efraim krug <efraimmkrug@gmail.com>
2018-12-20 13:09:03 -0500

    fixing config

--- ShellScripts/ConfigInstall.sh
+++ ShellScripts/ConfigInstall.sh
@@ -1,6 +1,10 @@
 cd $HOME/code/BeisChayim
-[ -n "$(find $HOME/Downloads -name 'BCConfig' | head -1)" ] || exit 0
-mv $HOME/Downloads/BCConfig $HOME/bcCode/BeisChayim/config/BCConfig
+X=0
+[ -n "$(find $HOME/Downloads -name 'BCConfig' | head -1)" ] || X=1
+[ -n "$(find $HOME/Downloads -name 'BCConfig.txt' | head -1)" ] || X=2
+if [ $X -eq 0 ];then exit 0; fi
+if [ $X -eq 1 ];then mv $HOME/Downloads/BCConfig $HOME/bcCode/BeisChayim/config/BCConfig; fi
+if [ $X -eq 2 ];then mv $HOME/Downloads/BCConfig.txt $HOME/bcCode/BeisChayim/config/BCConfig; fi
 # restart browser
 pkill -f chromium-browser
 chromium-browser --start-fullscreen $HOME/bcCode/BeisChayim/beisChayim.html &
