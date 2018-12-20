4ee7b04ff445cdee7e2072f0c7d50603f4a2d069
efraim krug <efraimmkrug@gmail.com>
2018-12-20 11:54:25 -0500

    cleaning up 10.1

--- python/ShellScripts/_ConfigInstall.sh
+++
@@ -1,5 +0,0 @@
-cd $HOME/code/BeisChayim
-[ -n "$(find $HOME/Downloads -name 'BCConfig' | head -1)" ] && mv $HOME/Downloads/BCConfig $HOME/bcCode/BeisChayim/config/BCConfig
-# restart browser
-pkill -f firefox
-firefox $HOME/bcCode/BeisChayim/BeisChayim.html &
