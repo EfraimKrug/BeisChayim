94c3b58d84625f00c0708c141be778f6e3633934
efraim krug <efraimmkrug@gmail.com>
2018-12-21 00:18:01 -0500

    install fix

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -2,9 +2,11 @@
 cd $HOME/bin
 cp $HOME/bcCode/BeisChayim/ShellScripts/*.sh $HOME/bin
 cp $HOME/bcCode/BeisChayim/ShellScripts/startUp $HOME/bin
+mv $HOME/bin/editConfig.sh editConfig
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
 mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
 
 chmod 555 $HOME/bin/BChCycle01
 chmod 555 $HOME/bin/ConfigInstall
 chmod 555 $HOME/bin/startUp
+chmod 555 $HOME/bin/editConfig
