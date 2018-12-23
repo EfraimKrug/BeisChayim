9016ae07b40731faa6cfcfbcf4cb6e2f6f62731b
efraim krug <efraimmkrug@gmail.com>
2018-12-23 14:39:50 -0500

    install fixed for crontab edit

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -5,8 +5,10 @@ cp $HOME/bcCode/BeisChayim/ShellScripts/startUp $HOME/bin
 mv $HOME/bin/editConfig.sh editConfig
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
 mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
+mv $HOME/bin/editCrontab.sh editCrontab
 
 chmod 555 $HOME/bin/BChCycle01
 chmod 555 $HOME/bin/ConfigInstall
 chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editConfig
+chmod 555 $HOME/bin/editCrontab
