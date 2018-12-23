944c7bfd1ebba9c0996d968c15a3149129e340ea
efraim krug <efraimmkrug@gmail.com>
2018-12-23 15:00:56 -0500

    fixing shell script

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -13,7 +13,7 @@ chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editConfig
 chmod 555 $HOME/bin/editCrontab
 
-cd $HOME/BeisChayim/python
+cd $HOME/bcCode/BeisChayim/python 
 sed -i -e 's#\/pi\/#\/efraim\/#g' cleanup01.py
 sed -i -e 's#\/pi\/#\/efraim\/#g' collect.py
 sed -i -e 's#\/pi\/#\/efraim\/#g' csv2jsond.py
