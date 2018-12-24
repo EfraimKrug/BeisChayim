2358c6f68b5f149be733139059c03e5afa3012ec
efraim krug <efraimmkrug@gmail.com>
2018-12-23 20:30:02 -0500

    fixing crontab edit

--- ShellScripts/editCrontab.sh
+++ ShellScripts/editCrontab.sh
@@ -1,7 +1,6 @@
-cd $HOME/code/BeisChayim/work
+cd $HOME/bcCode/BeisChayim/work
 cp /etc/crontab crontab.txt
-echo '*/2 * * * *      pi  export  DISPLAY=:0 && $HOME/bin/BChCycle01 &>$HOME/cron.log' >> crontab.txt
-echo '*/3 * * * *      pi  export  DISPLAY=:0 && $HOME/bin/ConfigInstall &>$HOME/cron.log' >> crontab.txt
+echo '*/2 * * * *      '$(whoami)  ' export  DISPLAY=:0 && $HOME/bin/BChCycle01 &>$HOME/cron.log' >> crontab.txt
+echo '*/3 * * * *      '$(whoami)  ' export  DISPLAY=:0 && $HOME/bin/ConfigInstall &>$HOME/cron.log' >> crontab.txt
 cat crontab.txt
 sudo cp crontab.txt /etc/crontab
-b0st0n
