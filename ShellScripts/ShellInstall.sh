6225adcc90bae98d316fa70c7646bd5a10b4fab7
efraim krug <efraimmkrug@gmail.com>
2018-12-26 20:08:05 -0500

    adding turnOn.sh

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -6,6 +6,7 @@ mv $HOME/bin/editConfig.sh editConfig
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
 mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
 mv $HOME/bin/editCrontab.sh editCrontab
+mv $HOME/bin/turnOn.sh turnOn
 
 rm $HOME/bin/ShellInstall.sh
 
@@ -14,8 +15,9 @@ chmod 555 $HOME/bin/ConfigInstall
 chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editConfig
 chmod 555 $HOME/bin/editCrontab
+chmod 555 $HOME/bin/turnOn
 
-cd $HOME/bcCode/BeisChayim/python 
+cd $HOME/bcCode/BeisChayim/python
 sed -i -e 's#@@@@@#'$HOME'#g'  cleanup01.py
 sed -i -e 's#@@@@@#'$HOME'#g' collect.py
 sed -i -e 's#@@@@@#'$HOME'#g' csv2jsond.py
---
+++ ShellScripts/turnOn.sh
@@ -0,0 +1,13 @@
+ff=$(update-alternatives --display gnome-www-browser | grep firefox | wc -l)
+if [ $ff -gt 0 ]
+then
+        pkill -f firefox
+        firefox  $HOME/bcCode/BeisChayim/beisChayim.html &
+fi
+
+ch=$(update-alternatives --display gnome-www-browser | grep chromium | wc -l)
+if [ $ch -gt 0 ]
+then
+        pkill -f chromium-browser
+        chromium-browser --start-fullscreen  $HOME/bcCode/BeisChayim/beisChayim.html &
+fi
