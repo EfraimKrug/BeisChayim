9e4d03173ce6f528a30ba34caf08f00b8e2f8614
efraim krug <efraimmkrug@gmail.com>
2019-02-18 21:17:55 -0500

    catching up

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -1,7 +1,7 @@
 # reinstall shell scripts
 cd $HOME/bin
 cp $HOME/$1/BeisChayim/ShellScripts/*.sh $HOME/bin
-cp $HOME/$1/BeisChayim/ShellScripts/startUp $HOME/bin
+#cp $HOME/$1/BeisChayim/ShellScripts/startUp $HOME/bin
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
 # mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
 mv $HOME/bin/editCrontab.sh editCrontab
@@ -15,7 +15,7 @@ rm $HOME/bin/ShellInstall.sh
 
 chmod 555 $HOME/bin/BChCycle01
 # chmod 555 $HOME/bin/ConfigInstall
-chmod 555 $HOME/bin/startUp
+#chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editCrontab
 chmod 555 $HOME/bin/turnOn
 chmod 555 $HOME/bin/cleanUp
