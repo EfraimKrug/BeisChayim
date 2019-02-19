2b56bbc2d0c0353d8c5ca5e8ec6b0fc526052ea1
efraim krug <efraimmkrug@gmail.com>
2019-02-19 17:59:39 -0500

    fixing Ccode references

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -1,9 +1,8 @@
 # reinstall shell scripts
 cd $HOME/bin
 cp $HOME/$1/BeisChayim/ShellScripts/*.sh $HOME/bin
-#cp $HOME/$1/BeisChayim/ShellScripts/startUp $HOME/bin
+
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
-# mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
 mv $HOME/bin/editCrontab.sh editCrontab
 mv $HOME/bin/turnOn.sh turnOn
 mv $HOME/bin/cleanUp.sh cleanUp
@@ -14,8 +13,6 @@ mv $HOME/bin/setView.sh setView
 rm $HOME/bin/ShellInstall.sh
 
 chmod 555 $HOME/bin/BChCycle01
-# chmod 555 $HOME/bin/ConfigInstall
-#chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editCrontab
 chmod 555 $HOME/bin/turnOn
 chmod 555 $HOME/bin/cleanUp
