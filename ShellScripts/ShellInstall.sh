d98aeab2d20970a719ea77c8cf78a304f05407ec
efraim krug <efraimmkrug@gmail.com>
2019-02-19 19:10:43 -0500

    should be

--- ShellScripts/ShellInstall.sh
+++ ShellScripts/ShellInstall.sh
@@ -1,9 +1,7 @@
 # reinstall shell scripts
 cd $HOME/bin
 cp $HOME/$1/BeisChayim/ShellScripts/*.sh $HOME/bin
-#cp $HOME/$1/BeisChayim/ShellScripts/startUp $HOME/bin
 mv $HOME/bin/BChCycle01.sh $HOME/bin/BChCycle01
-# mv $HOME/bin/ConfigInstall.sh $HOME/bin/ConfigInstall
 mv $HOME/bin/editCrontab.sh editCrontab
 mv $HOME/bin/turnOn.sh turnOn
 mv $HOME/bin/cleanUp.sh cleanUp
@@ -14,8 +12,6 @@ mv $HOME/bin/setView.sh setView
 rm $HOME/bin/ShellInstall.sh
 
 chmod 555 $HOME/bin/BChCycle01
-# chmod 555 $HOME/bin/ConfigInstall
-#chmod 555 $HOME/bin/startUp
 chmod 555 $HOME/bin/editCrontab
 chmod 555 $HOME/bin/turnOn
 chmod 555 $HOME/bin/cleanUp
@@ -25,7 +21,6 @@ chmod 555 $HOME/bin/setRun
 cd $HOME/$1/BeisChayim/python
 sed -i -e 's#X@~@~@X#'$HOME/$1'#g' cleanup01.py
 sed -i -e 's#X@~@~@X#'$HOME/$1'#g' collect.py
-sed -i -e 's#X@~@~@X#'$HOME/$1'#g' csv2jsond.py
 sed -i -e 's#X@~@~@X#'$HOME/$1'#g' csv2jsond.py
 sed -i -e 's#X@~@~@X#'$HOME/$1'#g' json2csv.py
 sed -i -e 's#X@~@~@X#'$HOME/$1'#g' readRunCheck.py
