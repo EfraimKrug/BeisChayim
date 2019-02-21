16fcc865b3d82de45d7314a81d5bfdd005b97afe
efraim krug <efraimmkrug@gmail.com>
2019-02-21 15:02:30 -0500

    decorating console

--- console/profile.py
+++ console/profile.py
@@ -2,3 +2,8 @@ bindir = r'X@-@X/bin'
 pythonDir = r'X@~@~@X/BeisChayim/python'
 consoleDir = r'X@~@~@X/BeisChayim/console'
 configDir = r'X@~@~@X/BeisChayim/config'
+
+# bindir = r'/home/efraim/bin'
+# pythonDir = r'/home/efraim/Ccode/BeisChayim/python'
+# consoleDir = r'/home/efraim/Ccode/BeisChayim/console'
+# configDir = r'/home/efraim/Ccode/BeisChayim/config'
