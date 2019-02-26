e6c701ba249f498f599ffea3cff811719efa114c
efraim krug <efraimmkrug@gmail.com>
2019-02-26 11:43:46 -0500

    changing files around

---
+++ console/old/readDB.py
@@ -0,0 +1,222 @@
+from tkinter import *
+from tkinter import ttk
+#from tkinter import filedialog
+#import tkinter.messagebox
+
+from PIL import ImageTk, Image
+#import subprocess
+#import os
+#from os import walk
+#import shutil
+
+from profile import *
+
+import json
+import pprint
+
+
+class dataInterface:
+    def __init__(self, var, data):
+        self.var = var
+        if self.var == 1:
+            self.buildData()
+        if self.var == 2:
+            self.data = data
+
+    def getData(self):
+        return self.data
+
+    def buildData(self):
+        lineCount = 0
+        JString = '{ "ENTRIES": ['
+        with open (jsDir + "db01.js", "r") as JSONfile:
+            for line in JSONfile:
+                if lineCount > 0:
+                    JString += line.replace("'","").replace("+","").replace(";","")
+                lineCount += 1
+
+        self.data = json.loads(JString)
+
+    def showData(self):
+        if self.var == 2:
+            return
+
+        for entry in self.data['ENTRIES']:
+            print(entry["ID"] + "==> " + entry["Name"])
+
+        pp = pprint.PrettyPrinter(indent=2)
+        pp.pprint(self.data)
+
+
+#########################################################
+class SearchNames:
+    def __init__(self, var, data):
+        self.var = var
+        self.targetArray = []
+        self.targetPointer = 0
+        self.targetName = ""
+        self.responseLabel = ""
+        self.outLabel = ""
+        self.data = data
+
+    def initialize(self):
+        self.targetArray = []
+
+    def setOutLabel(self, str):
+        self.outLabel = str
+
+    def getOutLabel(self):
+        return self.outLabel
+
+    def getLengthTargetArray(self):
+        return len(self.targetArray)
+
+    def getTargetEntry(self):
+        self.targetName = self.targetArray[self.targetPointer][1]
+        return self.targetName
+
+    def getTargetName(self):
+        self.targetName = self.targetArray[self.targetPointer][0]
+        return self.targetName
+
+    def searchName(self, str):
+        self.initialize()
+        for entry in self.data['ENTRIES']:
+            nArray = entry["Name"].split()
+            for eName in nArray:
+                if eName.lower().find(str.lower()) == 0:
+                    self.targetArray.append([entry["Name"], entry])
+
+        #print(self.targetArray[0][0])
+        self.setOutLabel(self.targetArray[0][0])
+
+    def getEachStroke(self, key):
+        if key.char.lower() == str('\b') and len(self.responseLabel) > 0:
+            self.responseLabel = self.responseLabel[0:len(self.esponseLabel)-1]
+
+        if key.char.lower() == str(' '):
+            self.responseLabel += key.char
+
+        if key.char.lower() >= str('a') and key.char.lower() <= str('z'):
+            self.responseLabel += key.char
+
+        self.searchName(self.responseLabel)
+
+    def getResponseLabel(self):
+        return self.responseLabel
+
+    def downArrow(self, key):
+        count = len(self.targetArray)
+        if count < 2:
+            return
+        if self.targetPointer > count - 2:
+            return
+        print("Incrementing")
+        self.targetPointer += 1
+        self.setOutlabel()
+
+    def upArrow(self, key):
+        count = len(self.targetArray)
+        if count < 2:
+            return
+        if self.targetPointer < 1:
+            return
+        print("Decrementing")
+        self.targetPointer -= 1
+        self.setOutlabel()
+
+#########################################################
+# var: 0 is data from a datafile
+# var: 1 is one entry of data
+#
+#########################################################
+
+class dataScreen:
+    def __init__(self, var, data):
+        self.dataI = dataInterface(var,data)
+        self.responseLabel = ""
+        self.outLabel = ""
+        self.var = var
+        self.root = Tk()
+        self.root.configure(bg="teal", pady=34, padx=17)
+        self.root.geometry('500x400')
+        self.frame = ""
+
+        if var == 1:
+            self.initOne()
+        if var == 2:
+            self.initTwo()
+
+    def initOne(self):
+        self.data = self.dataI.getData()
+        self.searchObj = SearchNames(34, self.data)
+        self.root.title('Install/Re-install Your Display')
+
+    def initTwo(self):
+        self.data = self.dataI.getData()
+        self.searchObj = SearchNames(34, self.data)
+        self.root.title('Edit The Entry')
+
+    def setStyle(self):
+        self.s = ttk.Style()
+        self.s.configure('.', background='lightblue', foreground='maroon', border='4', sticky=EW)
+        self.s.theme_use('default')
+
+    def clickResponse(self):
+        self.responseLabel['text'] = self.searchObj.getTargetName()
+        entry = self.searchObj.getTargetEntry()
+        print(entry['ID'])
+
+    def buildFrame(self, root):
+        self.frame = Frame(self.root, width=450, height=430)
+        self.frame.configure(bg="teal", pady=3, padx=3)
+
+        l1 = Label(self.frame, text="Search: ", bg="blue", fg="yellow", padx=4, pady=4)
+        l1.place(x=0, y=0, bordermode=OUTSIDE, height=30, width=150)
+        self.frame.pack()
+
+    def getEachKeyStroke(self, key):
+        self.searchObj.getEachStroke(key)
+        self.setOutlabel()
+
+    def placeTextBox(self, frame):
+        self.txt = Entry(frame, width=45,  borderwidth=2, relief="sunken")
+        self.txt.place(x=210, y=33, bordermode=OUTSIDE, height=30, width=150)
+        self.txt.bind("<Key>", self.getEachKeyStroke)
+        self.txt.bind("<Up>", self.searchObj.upArrow)
+        self.txt.bind("<Down>", self.searchObj.downArrow)
+        self.txt.focus()
+        return self.txt
+
+    def placeButton(self, frame):
+        self.button0 = ttk.Button(self.frame, text="Click It", command =self.clickResponse)
+        self.button0.pack(side=RIGHT)
+        self.button0.place(x=310, y=350, bordermode=OUTSIDE, height=30, width=150)
+        #return self.button0
+
+    def placeResponseLabel(self, frame):
+        self.responseLabel = Label(self.frame, text="Response Label", bg="blue", fg="yellow", padx=4, pady=4)
+        self.responseLabel.place(x=30, y=120, bordermode=OUTSIDE, height=30, width=150)
+        return self.responseLabel
+
+    def placeOutLabel(self, frame):
+        OutLabel = Label(self.frame, text="Result Label", bg="blue", fg="yellow", padx=4, pady=4)
+        OutLabel.place(x=30, y=170, bordermode=OUTSIDE, height=30, width=450)
+        return OutLabel
+
+    def setOutlabel(self):
+        self.outLabel['text'] = self.searchObj.getOutLabel()
+
+    def runScreen(self):
+        frame = self.buildFrame(self.root)
+        self.placeButton(frame)
+        print(self.button0['text'])
+        responseLabel = self.placeResponseLabel(frame)
+        self.outLabel = self.placeOutLabel(frame)
+        textBox = self.placeTextBox(frame)
+        self.root.mainloop()
+
+############################################################################################
+
+dScreen = dataScreen(1, 0)
+dScreen.runScreen()
--- console/readDB.py
+++ console/readDB.py
@@ -1,222 +1,289 @@
-from tkinter import *
-from tkinter import ttk
-#from tkinter import filedialog
-#import tkinter.messagebox
+import tkinter as tk
+from tkinter import filedialog
+from dataInterface import *
+from SearchNames import *
+import tkinter.scrolledtext as tkst
 
-from PIL import ImageTk, Image
-#import subprocess
-#import os
-#from os import walk
-#import shutil
-
-from profile import *
-
-import json
-import pprint
-
-
-class dataInterface:
-    def __init__(self, var, data):
-        self.var = var
-        if self.var == 1:
-            self.buildData()
-        if self.var == 2:
-            self.data = data
-
-    def getData(self):
-        return self.data
-
-    def buildData(self):
-        lineCount = 0
-        JString = '{ "ENTRIES": ['
-        with open (jsDir + "db01.js", "r") as JSONfile:
-            for line in JSONfile:
-                if lineCount > 0:
-                    JString += line.replace("'","").replace("+","").replace(";","")
-                lineCount += 1
-
-        self.data = json.loads(JString)
-
-    def showData(self):
-        if self.var == 2:
-            return
-
-        for entry in self.data['ENTRIES']:
-            print(entry["ID"] + "==> " + entry["Name"])
-
-        pp = pprint.PrettyPrinter(indent=2)
-        pp.pprint(self.data)
-
-
-#########################################################
-class SearchNames:
-    def __init__(self, var, data):
-        self.var = var
-        self.targetArray = []
-        self.targetPointer = 0
-        self.targetName = ""
-        self.responseLabel = ""
-        self.outLabel = ""
-        self.data = data
-
-    def initialize(self):
-        self.targetArray = []
-
-    def setOutLabel(self, str):
-        self.outLabel = str
-
-    def getOutLabel(self):
-        return self.outLabel
-
-    def getLengthTargetArray(self):
-        return len(self.targetArray)
-
-    def getTargetEntry(self):
-        self.targetName = self.targetArray[self.targetPointer][1]
-        return self.targetName
-
-    def getTargetName(self):
-        self.targetName = self.targetArray[self.targetPointer][0]
-        return self.targetName
-
-    def searchName(self, str):
-        self.initialize()
-        for entry in self.data['ENTRIES']:
-            nArray = entry["Name"].split()
-            for eName in nArray:
-                if eName.lower().find(str.lower()) == 0:
-                    self.targetArray.append([entry["Name"], entry])
-
-        #print(self.targetArray[0][0])
-        self.setOutLabel(self.targetArray[0][0])
-
-    def getEachStroke(self, key):
-        if key.char.lower() == str('\b') and len(self.responseLabel) > 0:
-            self.responseLabel = self.responseLabel[0:len(self.esponseLabel)-1]
-
-        if key.char.lower() == str(' '):
-            self.responseLabel += key.char
-
-        if key.char.lower() >= str('a') and key.char.lower() <= str('z'):
-            self.responseLabel += key.char
-
-        self.searchName(self.responseLabel)
-
-    def getResponseLabel(self):
-        return self.responseLabel
-
-    def downArrow(self, key):
-        count = len(self.targetArray)
-        if count < 2:
-            return
-        if self.targetPointer > count - 2:
-            return
-        print("Incrementing")
-        self.targetPointer += 1
-        self.setOutlabel()
-
-    def upArrow(self, key):
-        count = len(self.targetArray)
-        if count < 2:
-            return
-        if self.targetPointer < 1:
-            return
-        print("Decrementing")
-        self.targetPointer -= 1
-        self.setOutlabel()
-
-#########################################################
-# var: 0 is data from a datafile
-# var: 1 is one entry of data
-#
-#########################################################
-
-class dataScreen:
-    def __init__(self, var, data):
-        self.dataI = dataInterface(var,data)
-        self.responseLabel = ""
-        self.outLabel = ""
-        self.var = var
-        self.root = Tk()
-        self.root.configure(bg="teal", pady=34, padx=17)
-        self.root.geometry('500x400')
-        self.frame = ""
-
-        if var == 1:
-            self.initOne()
-        if var == 2:
-            self.initTwo()
-
-    def initOne(self):
+class Demo1:
+    def __init__(self, master):
+        self.dataI = dataInterface(1, [])
         self.data = self.dataI.getData()
         self.searchObj = SearchNames(34, self.data)
-        self.root.title('Install/Re-install Your Display')
 
-    def initTwo(self):
-        self.data = self.dataI.getData()
-        self.searchObj = SearchNames(34, self.data)
-        self.root.title('Edit The Entry')
+        self.master = master
+        self.master.configure(bg="teal", pady=34, padx=17)
+        self.master.geometry('500x400')
+        self.master.title('Install/Re-install Your Display')
 
-    def setStyle(self):
-        self.s = ttk.Style()
-        self.s.configure('.', background='lightblue', foreground='maroon', border='4', sticky=EW)
-        self.s.theme_use('default')
-
-    def clickResponse(self):
-        self.responseLabel['text'] = self.searchObj.getTargetName()
-        entry = self.searchObj.getTargetEntry()
-        print(entry['ID'])
-
-    def buildFrame(self, root):
-        self.frame = Frame(self.root, width=450, height=430)
+        self.frame = tk.Frame(self.master, width=450, height=430)
         self.frame.configure(bg="teal", pady=3, padx=3)
+        self.frame.grid(row=1, column=1)
 
-        l1 = Label(self.frame, text="Search: ", bg="blue", fg="yellow", padx=4, pady=4)
-        l1.place(x=0, y=0, bordermode=OUTSIDE, height=30, width=150)
-        self.frame.pack()
+        self.l1 = tk.Label(self.frame, text="Search: ", bg="teal", fg="yellow")
+        self.l1.grid(row=1, column=2, padx=4, pady=4, sticky=tk.W)
+
+        self.button0 = tk.Button(self.frame, text="Click It", command =self.new_window)
+        self.button0.grid(row=1, column=12, columnspan=4, padx=4, pady=4, sticky=tk.E)
+
+        self.txt = tk.Entry(self.frame, width=45,  borderwidth=2, relief="sunken")
+        self.txt.place(x=210, y=210, bordermode=tk.OUTSIDE, height=30, width=150)
+        self.txt.bind("<Key>", self.getEachKeyStroke)
+        self.txt.bind("<Up>", self.upArrow)
+        self.txt.bind("<Down>", self.downArrow)
+        self.txt.focus()
+        self.txt.grid(row=4, column=2, columnspan=4, padx=4, pady=4, sticky=tk.E)
+
+        self.responseLabel = tk.Label(self.frame, text="Response Label", bg="teal", fg="yellow")
+        self.responseLabel.grid(row=7, rowspan=2, column=1, columnspan=4, padx=4, pady=4, sticky=tk.W)
+
+        self.OutLabel = tk.Label(self.frame, text="Result Label", bg="teal", fg="yellow")
+        self.OutLabel.grid(row=9, rowspan=2, column=1, columnspan=4, padx=4, pady=4, sticky=tk.W)
+
 
     def getEachKeyStroke(self, key):
+        #print("getEachKeyStroke")
         self.searchObj.getEachStroke(key)
-        self.setOutlabel()
+        self.setOutlabel(self.searchObj.getTargetName())
 
-    def placeTextBox(self, frame):
-        self.txt = Entry(frame, width=45,  borderwidth=2, relief="sunken")
-        self.txt.place(x=210, y=33, bordermode=OUTSIDE, height=30, width=150)
-        self.txt.bind("<Key>", self.getEachKeyStroke)
-        self.txt.bind("<Up>", self.searchObj.upArrow)
-        self.txt.bind("<Down>", self.searchObj.downArrow)
-        self.txt.focus()
-        return self.txt
+    def setOutlabel(self, newLabel):
+        self.OutLabel['text'] = newLabel
+        #self.OutLabel['text'] = self.searchObj.getOutLabel()
 
-    def placeButton(self, frame):
-        self.button0 = ttk.Button(self.frame, text="Click It", command =self.clickResponse)
-        self.button0.pack(side=RIGHT)
-        self.button0.place(x=310, y=350, bordermode=OUTSIDE, height=30, width=150)
-        #return self.button0
+    def upArrow(self, key):
+        #print("upArrow")
+        self.searchObj.upArrow(key, self.setOutlabel)
 
-    def placeResponseLabel(self, frame):
-        self.responseLabel = Label(self.frame, text="Response Label", bg="blue", fg="yellow", padx=4, pady=4)
-        self.responseLabel.place(x=30, y=120, bordermode=OUTSIDE, height=30, width=150)
-        return self.responseLabel
+    def downArrow(self, key):
+        #print("downArrow")
+        self.searchObj.downArrow(key, self.setOutlabel)
 
-    def placeOutLabel(self, frame):
-        OutLabel = Label(self.frame, text="Result Label", bg="blue", fg="yellow", padx=4, pady=4)
-        OutLabel.place(x=30, y=170, bordermode=OUTSIDE, height=30, width=450)
-        return OutLabel
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        #print(self.searchObj.getTargetEntry())
+        self.app = Demo2(self.newWindow, self.searchObj.getTargetEntry())
+#############################################################
+class Demo2:
+    def __init__(self, master, data):
+        self.master = master
+        self.master.configure(bg="teal", pady=34, padx=17)
+        self.master.geometry('900x600')
+        self.master.title(data['Name'])
 
-    def setOutlabel(self):
-        self.outLabel['text'] = self.searchObj.getOutLabel()
+        #self.frame = tk.Frame(self.master)
+        self.frame = tk.Frame(self.master, width=850, height=530)
+        self.frame.configure(bg="teal", pady=3, padx=3)
+        self.frame.grid(row=1, column=1)
 
-    def runScreen(self):
-        frame = self.buildFrame(self.root)
-        self.placeButton(frame)
-        print(self.button0['text'])
-        responseLabel = self.placeResponseLabel(frame)
-        self.outLabel = self.placeOutLabel(frame)
-        textBox = self.placeTextBox(frame)
-        self.root.mainloop()
+        self.l = tk.Label(self.frame, text=data['HName'], bg="teal", fg="yellow")
+        self.l.grid(row=1, column=2, padx=4, pady=4, sticky=tk.W)
 
-############################################################################################
+        self.l0 = tk.Label(self.frame, text="English Name: ", bg="teal", fg="yellow")
+        self.l0.grid(row=2, column=1, padx=4, pady=4, sticky=tk.W)
+        self.txt0 = tk.Entry(self.frame, width=25,  borderwidth=2, relief="sunken")
+        self.txt0.focus()
+        self.txt0.grid(row=2, column=3, columnspan=2, padx=4, pady=4, sticky=tk.E)
+        self.txt0.insert(0, data['Name'])
 
-dScreen = dataScreen(1, 0)
-dScreen.runScreen()
+
+        self.l1 = tk.Label(self.frame, text="Hebrew Name: ", bg="teal", fg="yellow")
+        self.l1.grid(row=3, column=1, padx=4, pady=4, sticky=tk.W)
+        self.txt1 = tk.Entry(self.frame, width=25,  borderwidth=2, relief="sunken")
+        self.txt1.focus()
+        self.txt1.grid(row=3, column=3, columnspan=2, padx=4, pady=4, sticky=tk.E)
+        self.txt1.insert(0, data['HName'])
+
+        self.l2 = tk.Label(self.frame, text="Gregorian Date: ", bg="teal", fg="yellow")
+        self.l2.grid(row=4, column=1, padx=4, pady=4, sticky=tk.W)
+        self.txt2 = tk.Entry(self.frame, width=25,  borderwidth=2, relief="sunken")
+        self.txt2.focus()
+        self.txt2.grid(row=4, column=3, columnspan=2, padx=4, pady=4, sticky=tk.E)
+        self.txt2.insert(0, data['EDate'])
+
+        self.l3 = tk.Label(self.frame, text="Hebrew Date: ", bg="teal", fg="yellow")
+        self.l3.grid(row=5, column=1, padx=4, pady=4, sticky=tk.W)
+        self.txt3 = tk.Entry(self.frame, width=25,  borderwidth=2, relief="sunken")
+        self.txt3.focus()
+        self.txt3.grid(row=5, column=3, columnspan=2, padx=4, pady=4, sticky=tk.E)
+        self.txt3.insert(0, data['HDate'])
+
+
+        # self.responseLabel = tk.Label(self.frame, text="Response Label", bg="teal", fg="yellow")
+        # self.responseLabel.grid(row=7, rowspan=2, column=1, columnspan=4, padx=4, pady=4, sticky=tk.W)
+        #
+        # self.OutLabel = tk.Label(self.frame, text="Result Label", bg="teal", fg="yellow")
+        # self.OutLabel.grid(row=9, rowspan=2, column=1, columnspan=4, padx=4, pady=4, sticky=tk.W)
+        #
+        payAmounts = [1,2,3,4]
+        variable = tk.StringVar(self.frame)
+        idx = payAmounts.index(int(data['PayLevel']))
+        variable.set(payAmounts[idx]) # default value
+
+        self.l4 = tk.Label(self.frame, text="Pay Level: ", bg="teal", fg="yellow")
+        self.l4.grid(row=6, column=1, padx=4, pady=4, sticky=tk.W)
+
+        self.dropDown4 =tk.OptionMenu(self.frame, variable, *payAmounts)
+        self.dropDown4.config(bg = "teal", relief="raised")
+        self.dropDown4.grid(row=6, column=3,  columnspan=2, padx=4, pady=4, sticky=tk.E)
+
+        self.l5 = tk.Label(self.frame, text="Comment Text: ", bg="teal", fg="yellow")
+        self.l5.grid(row=7, column=1, padx=4, pady=4, sticky=tk.W)
+        self.textfield5 = tkst.Text(self.frame, wrap=tk.WORD, height=6, width=40 )
+        self.textfield5.grid(row=7, rowspan=2, column=2, columnspan=2, padx=4, pady=4, sticky=tk.NSEW)
+        self.textfield5.insert(tk.INSERT, data['Comments01'].strip())
+
+        self.scrollbar = tkst.Scrollbar(self.frame, command=self.textfield5.yview)
+        self.textfield5.configure(yscrollcommand=self.scrollbar.set)
+        #self.scrollbar.config(self.textfield5.yview)
+        #self.grid()
+        self.scrollbar.grid(column=4, row=7, rowspan=2,  sticky=tk.NS)
+
+        txt = data['Pic01'] or "Nothing..."
+        self.btn6 = tk.Button(self.frame, text="Get File", command=self.fileSelectv6)
+        self.btn6.grid(row=3, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l6 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l6.grid(row=3, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['Pic02'] or "Nothing..."
+        self.btn7 = tk.Button(self.frame, text="Get File", command=self.fileSelectv7)
+        self.btn7.grid(row=4, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l7 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l7.grid(row=4, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['PDF01'] or "Nothing..."
+        self.btn8 = tk.Button(self.frame, text="Get File", command=self.fileSelectv8)
+        self.btn8.grid(row=5, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l8 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l8.grid(row=5, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['PDF02'] or "Nothing..."
+        self.btn9 = tk.Button(self.frame, text="Get File", command=self.fileSelectv9)
+        self.btn9.grid(row=6, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l9 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l9.grid(row=6, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['PDF03'] or "Nothing..."
+        self.btn10 = tk.Button(self.frame, text="Get File", command=self.fileSelectv10)
+        self.btn10.grid(row=7, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l10 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l10.grid(row=7, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['PDF04'] or "Nothing..."
+        self.btn11 = tk.Button(self.frame, text="Get File", command=self.fileSelectv11)
+        self.btn11.grid(row=8, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l11 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l11.grid(row=8, column=6, padx=4, pady=4, sticky=tk.W)
+
+        txt = data['PDF05'] or "Nothing..."
+        self.btn12 = tk.Button(self.frame, text="Get File", command=self.fileSelectv12)
+        self.btn12.grid(row=9, column=5, padx=4, pady=4, sticky=tk.W)
+        self.l12 = tk.Label(self.frame, text=txt, bg="teal", fg="yellow")
+        self.l12.grid(row=9, column=6, padx=4, pady=4, sticky=tk.W)
+
+        self.saveButton = tk.Button(self.frame, text="Save", width = 13, command =self.saveData)
+        self.saveButton.grid(row=13, column=3, padx=4, pady=4, sticky=tk.E)
+
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 13, command = self.close_windows)
+        self.quitButton.grid(row=13, column=2, padx=4, pady=4, sticky=tk.E)
+
+        #print(data)
+
+    def fileSelectv6(self):
+        self.fileSelect(6)
+
+    def fileSelectv7(self):
+        self.fileSelect(7)
+
+    def fileSelectv8(self):
+        self.fileSelect(8)
+
+    def fileSelectv9(self):
+        self.fileSelect(9)
+
+    def fileSelectv10(self):
+        self.fileSelect(10)
+
+    def fileSelectv11(self):
+        self.fileSelect(11)
+
+    def fileSelectv12(self):
+        self.fileSelect(12)
+
+
+    def fileSelect(self, choice):
+        self.pic01 =  filedialog.askopenfilename(initialdir = "/home/efraiim/code/BeisChayim/img",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        if choice == 6:
+            self.l6.configure(text=self.pic01)
+        if choice == 7:
+            self.l7.configure(text=self.pic01)
+        if choice == 8:
+            self.l8.configure(text=self.pic01)
+        if choice == 9:
+            self.l9.configure(text=self.pic01)
+        if choice == 10:
+            self.l10.configure(text=self.pic01)
+        if choice == 11:
+            self.l11.configure(text=self.pic01)
+        if choice == 12:
+            self.l12.configure(text=self.pic01)
+
+        #print (self.pic01)
+
+    def saveData(self):
+        #data['Name'] = self.txt0.get()
+        #data['HName'] = self.txt1.get()
+        #data['EDate'] = self.txt2.get()
+        #data['HDate'] = self.txt3.get()
+        #data['Comments01'] = self.textfield5.get()
+        #data['Pic01'] = self.l6.cget('text')
+        #data['Pic02'] = self.l7.cget('text')
+        #data['PDF01'] = self.l8.cget('text')
+        #data['PDF02'] = self.l9.cget('text')
+        #data['PDF03'] = self.l10.cget('text')
+        #data['PDF04'] = self.l11.cget('text')
+        #data['PDF05'] = self.l12.cget('text')
+
+        self.printl(self.txt0.get())
+        self.printl(self.txt1.get())
+        self.printl(self.txt2.get())
+        self.printl(self.txt3.get())
+        self.printl(self.textfield5.get("1.0", tk.END))
+
+        self.printl(self.l6.cget('text'))
+        self.printl(self.l7.cget('text'))
+        self.printl(self.l8.cget('text'))
+        self.printl(self.l9.cget('text'))
+        self.printl(self.l10.cget('text'))
+        self.printl(self.l11.cget('text'))
+        self.printl(self.l12.cget('text'))
+
+    def printl(self, text):
+        print("[" + text + "]")
+
+    def getEachKeyStroke(self, key):
+        #print("getEachKeyStroke")
+        self.searchObj.getEachStroke(key)
+        self.setOutlabel(self.searchObj.getTargetName())
+
+    def setOutlabel(self, newLabel):
+        self.OutLabel['text'] = newLabel
+        #self.OutLabel['text'] = self.searchObj.getOutLabel()
+
+    def upArrow(self, key):
+        #print("upArrow")
+        self.searchObj.upArrow(key, self.setOutlabel)
+
+    def downArrow(self, key):
+        #print("downArrow")
+        self.searchObj.downArrow(key, self.setOutlabel)
+
+
+    def close_windows(self):
+        self.master.destroy()
+
+def main():
+    root = tk.Tk()
+    app = Demo1(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
