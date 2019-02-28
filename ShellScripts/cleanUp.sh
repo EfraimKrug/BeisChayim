db017e8d529e7a191adba9485d5a50dcdcf9674f
efraim krug <efraimmkrug@gmail.com>
2019-02-27 19:38:45 -0500

    readjusting stuff

--- ShellScripts/cleanUp.sh
+++ ShellScripts/cleanUp.sh
@@ -6,7 +6,8 @@ mkdir $TEMP_DIRECTORY/config
 cp $HOME/$CODE_DIRECTORY/BeisChayim/config/* $TEMP_DIRECTORY/config
 cp $HOME/$CODE_DIRECTORY/BeisChayim/js/db01.js $HOME/$TEMP_DIRECTORY/db01.js
 cp $HOME/$CODE_DIRECTORY/BeisChayim/data/yahrzeits.csv $HOME/$TEMP_DIRECTORY/yahrzeits.csv
-python3 $HOME/$CODE_DIRECTORY/BeisChayim/python/json2csv.py
+cd $HOME/$CODE_DIRECTORY/BeisChayim/python
+python3 json2csv.py
 mv nYahrzeits.csv $HOME/$TEMP_DIRECTORY/nYahrzeits.csv
 cp $HOME/bin/appInstall $HOME/$TEMP_DIRECTORY/appInstall
 rm -f -r $CODE_DIRECTORY
