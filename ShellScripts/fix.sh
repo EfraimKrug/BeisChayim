a1c35f5b48b77177a3c53937be603e46a0e274c5
efraim krug <efraimmkrug@gmail.com>
2019-03-29 12:34:15 -0400

    cleaning out some old files

--- ShellScripts/fix.sh
+++
@@ -1,2 +0,0 @@
-CODE_DIRECTORY=bcCode
-python3 $HOME/$CODE_DIRECTORY/BeisChayim/console/readDB.py
--- ShellScripts/setRun.sh
+++
@@ -1,4 +0,0 @@
-CODE_DIRECTORY=bcCode
-echo 'var RunPhase = { "phase01": {"run_type":"security"}};' > $HOME/$CODE_DIRECTORY/BeisChayim/config/RunPhase
-echo '//var RunPhase = { "phase01": {"run_type":"view"}};'  >> $HOME/$CODE_DIRECTORY/BeisChayim/config/RunPhase
-$HOME/bin/turnOn $CODE_DIRECTORY
