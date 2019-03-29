e2cb612f1adbd44a88066d1777e68a61f955b337
efraim krug <efraimmkrug@gmail.com>
2019-03-29 11:17:59 -0400

    control char in file processing

--- python/csv2jsond.py
+++ python/csv2jsond.py
@@ -1,5 +1,13 @@
 import csv
 
+def setField(field):
+	first = str(field)
+	f1 = first.replace("&quot;","'")
+	f2 = f1.replace("&quot;",'"')
+	f3 = f2.replace("&comma;",",")
+	f4 = f3.replace("&bslash;","\\")
+	return f4
+
 def recycledLine(line):
 	if len(line[2]) == 0:
 		return False
@@ -23,15 +31,15 @@ with open('X@~@~@X/BeisChayim/data/out01') as csvfile:
 		l2 = list(x)
 		if not l2:
 			continue
-		newline.append("'{\"ID\":\"" + l2[0][0] + "\"")
+		newline.append("'{\"ID\":\"" + setField(l2[0][0]) + "\"")
 		#newline.append("\"BGround\":\"\"")
 		if len(l2[1]) > 0:
-			newline.append("\"Name\":\"" + l2[1][0] + "\"")
+			newline.append("\"Name\":\"" + setField(l2[1][0]) + "\"")
 		else:
 			newline.append("\"Name\":\"\"")
 
 		if len(l2[4]) > 0:
-			newline.append("\"HName\":\"" + l2[4][0] + "\"")
+			newline.append("\"HName\":\"" + setField(l2[4][0]) + "\"")
 		else:
 			newline.append("\"HName\":\"\"")
 
@@ -49,7 +57,7 @@ with open('X@~@~@X/BeisChayim/data/out01') as csvfile:
 			newline.append("\"EDate\":\"\"")
 
 		if len(l2[7]) > 0:
-			newline.append("\"HDate\":\"" + l2[7][0] + "\"")
+			newline.append("\"HDate\":\"" + setField(l2[7][0]) + "\"")
 		else:
 			newline.append("\"HDate\":\"\"")
 
@@ -57,7 +65,7 @@ with open('X@~@~@X/BeisChayim/data/out01') as csvfile:
 			newline.append("\"MournBy\":\"\"")
 		else:
 			if len(l2[24]) > 0:
-				newline.append("\"MournBy\":\"" + l2[24][0] + "\"")
+				newline.append("\"MournBy\":\"" + setField(l2[24][0]) + "\"")
 			else:
 				newline.append("\"MournBy\":\"\"")
 
@@ -65,54 +73,54 @@ with open('X@~@~@X/BeisChayim/data/out01') as csvfile:
 			newline.append("\"Relationship\":\"\"")
 		else:
 			if len(l2[27]) > 0:
-				newline.append("\"Relationship\":\"" + l2[27][0] + "\"")
+				newline.append("\"Relationship\":\"" + setField(l2[27][0]) + "\"")
 			else:
 				newline.append("\"Relationship\":\"\"")
 
 
 		if recycledLine(l2):
 			if len(l2[29]) > 0:
-				newline.append("\"Pic01\":\"" + l2[29][0] + "\"")
+				newline.append("\"Pic01\":\"" + setField(l2[29][0]) + "\"")
 			else:
 				newline.append("\"Pic01\":\"\"")
 
 			if len(l2[30]) > 0:
-				newline.append("\"Pic02\":\"" + l2[30][0] + "\"")
+				newline.append("\"Pic02\":\"" + setField(l2[30][0]) + "\"")
 			else:
 				newline.append("\"Pic02\":\"\"")
 
 			if len(l2[31]) > 0:
-				newline.append("\"PDF01\":\"" + l2[31][0] + "\"")
+				newline.append("\"PDF01\":\"" + setField(l2[31][0]) + "\"")
 			else:
 				newline.append("\"PDF01\":\"\"")
 
 			if len(l2[32]) > 0:
-				newline.append("\"PDF02\":\"" + l2[32][0] + "\"")
+				newline.append("\"PDF02\":\"" + setField(l2[32][0]) + "\"")
 			else:
 				newline.append("\"PDF02\":\"\"")
 
 			if len(l2[33]) > 0:
-				newline.append("\"PDF03\":\"" + l2[33][0] + "\"")
+				newline.append("\"PDF03\":\"" + setField(l2[33][0]) + "\"")
 			else:
 				newline.append("\"PDF03\":\"\"")
 
 			if len(l2[34]) > 0:
-				newline.append("\"PDF04\":\"" + l2[34][0] + "\"")
+				newline.append("\"PDF04\":\"" + setField(l2[34][0]) + "\"")
 			else:
 				newline.append("\"PDF04\":\"\"")
 
 			if len(l2[35]) > 0:
-				newline.append("\"PDF05\":\"" + l2[35][0] + "\"")
+				newline.append("\"PDF05\":\"" + setField(l2[35][0]) + "\"")
 			else:
 				newline.append("\"PDF05\":\"\"")
 
 			if len(l2[36]) > 0:
-				newline.append("\"PayLevel\":\"" + l2[36][0] + "\"")
+				newline.append("\"PayLevel\":\"" + setField(l2[36][0]) + "\"")
 			else:
 				newline.append("\"PayLevel\":\"\"")
 
 			if len(l2[37]) > 0:
-				newline.append("\"Comments01\":\"" + l2[37][0] + "\"")
+				newline.append("\"Comments01\":\"" + setField(l2[37][0]) + "\"")
 			else:
 				newline.append("\"Comments01\":\"\"")
 		else:
@@ -125,7 +133,6 @@ with open('X@~@~@X/BeisChayim/data/out01') as csvfile:
 			newline.append("\"PDF05\":\"\"")
 			newline.append("\"PayLevel\":\"\"")
 			newline.append("\"Comments01\":\"\"")
-
 
 		if linecount > 0:
 			if linecount < len(l):
